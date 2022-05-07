1f75b2db8b442108167e04ac2105462c7c2f5b5d
Austin Brock <austinjbrock@gmail.com>
2022-05-06 20:32:15 -0700

    added files

---
+++ app.js
@@ -0,0 +1,22 @@
+let choices = ['rock','paper','scissors']
+
+function game(){
+
+}
+
+
+function playRound(playerSelection,computerSelection){
+
+}
+
+function playerChoice (){
+   const playerHand = 'rock'
+}
+
+function computerChoice(){
+   //gives a random choice from the computer
+   return choices[Math.floor(Math.random()* choices.length)]
+}
+
+console.log(computerChoice())
+
