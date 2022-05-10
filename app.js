8899f28c55713009e1334c797c6a41df702a4566
Austin Brock <austinjbrock@gmail.com>
2022-05-09 18:36:45 -0700

    finished game logic

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
-let choices = ['rock','paper','scissors']
-
-function game(){
-
-}
-
-
-function playRound(playerSelection,computerSelection){
-
-}
+const choices = ['rock','paper','scissors']
 
 function playerChoice (){
-   const playerHand = 'rock'
+   let input = prompt('Enter Rock, Paper, or Scissors')
+   input = input.toLowerCase()
+   return input
+} 
+
+ function computerChoice(){
+//gets a random choice from the computer
+  return choices[Math.floor(Math.random()* choices.length)]
+ }
+
+
+function playRound(){
+
+const computerSelection = computerChoice()
+const playerSelection = playerChoice()
+
+
+if (playerSelection === "rock") {
+   if (computerSelection === "rock") {
+       console.log( "Draw!");
+   } else if (computerSelection === "paper") {
+       console.log( "Computer wins!");
+   } else {
+       console.log( "Player wins!");
+   }
+} else if (playerSelection === "paper") {
+   if (computerSelection === "rock") {
+       console.log( "Player wins!");
+   } else if (computerSelection === "paper") {
+       console.log( "Draw!");
+   } else {
+       console.log( "Computer wins!");
+   }
+} else {
+   if (computerSelection === "rock") {
+       console.log( "Computer wins!");
+   } else if (computerSelection === "paper") {
+       console.log( "Player wins!");
+   } else {
+       console.log( "Draw!");
+   }
 }
 
-function computerChoice(){
-   //gives a random choice from the computer
-   return choices[Math.floor(Math.random()* choices.length)]
+console.log(`player chose ${playerSelection}`)
+console.log(`Computer Chose ${computerSelection}`)
+
 }
 
-console.log(computerChoice())
+
+
+playRound(playerChoice,computerChoice)
+
+
 
