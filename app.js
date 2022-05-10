e09504d61b14f552d2d6ea0ac8152f61e046d4ce
Austin Brock <austinjbrock@gmail.com>
2022-05-09 19:21:30 -0700

    cleaned up js file for readability

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const choices = ['rock','paper','scissors']
 
+//Grab user input
 function playerChoice (){
    let input = prompt('Enter Rock, Paper, or Scissors')
    input = input.toLowerCase()
    return input
 } 
-
+// Get Computer input
  function computerChoice(){
-//gets a random choice from the computer
   return choices[Math.floor(Math.random()* choices.length)]
  }
 
-
+// 
 function playRound(){
 
 const computerSelection = computerChoice()
@@ -20,37 +20,41 @@ const playerSelection = playerChoice()
 
 if (playerSelection === "rock") {
    if (computerSelection === "rock") {
-       console.log( "Draw!");
+       console.log( "Draw!")
    } else if (computerSelection === "paper") {
-       console.log( "Computer wins!");
+       console.log( "Computer wins!")
    } else {
-       console.log( "Player wins!");
+       console.log( "Player wins!")
    }
+
 } else if (playerSelection === "paper") {
    if (computerSelection === "rock") {
-       console.log( "Player wins!");
+       console.log( "Player wins!")
    } else if (computerSelection === "paper") {
-       console.log( "Draw!");
+       console.log( "Draw!")
    } else {
-       console.log( "Computer wins!");
+       console.log( "Computer wins!")
    }
+
 } else {
    if (computerSelection === "rock") {
-       console.log( "Computer wins!");
+       console.log( "Computer wins!")
    } else if (computerSelection === "paper") {
-       console.log( "Player wins!");
+       console.log( "Player wins!")
    } else {
-       console.log( "Draw!");
+       console.log( "Draw!")
    }
+
+
+
 }
 
 console.log(`player chose ${playerSelection}`)
 console.log(`Computer Chose ${computerSelection}`)
 
-}
+}// End PlayRound Function
 
-
-
+//Call the playRound Function to start score
 playRound(playerChoice,computerChoice)
 
 
